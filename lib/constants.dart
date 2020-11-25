aec6195a63db17c84f9a8482b0a27b2faebeff8f
Yousaf <muhammadyousafg51@gmail.com>
2020-11-25 21:33:45 +0500

    colors update

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
 
-const kBottomContainerHeight = 80.0;
-const kActiveCardColour = Color(0xFF1D1E33);
-const kInactiveCardColour = Color(0xFF111328);
-const kBottomContainerColour = Color(0xFFEB1555);
+
+const kBottomContainerHeight = 70.0;
+const kActiveCardColour = Colors.white;
+const kInactiveCardColour = Colors.black12;
 
 const kLabelTextStyle = TextStyle(
   fontSize: 18.0,
