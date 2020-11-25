c94edf3743725282d70f7f216d76333a4959e9a5
Yousaf <muhammadyousafg51@gmail.com>
2020-11-25 21:28:28 +0500

    result manage

--- lib/screens/results_page.dart
+++ lib/screens/results_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'package:flutter_neumorphic/flutter_neumorphic.dart';
 import '../components/bottom_button.dart';
 import '../components/reusable_card.dart';
 import '../constants.dart';
@@ -7,8 +7,8 @@ import '../constants.dart';
 class ResultsPage extends StatelessWidget {
   ResultsPage(
       {@required this.bmiResult,
-      @required this.interpretation,
-      @required this.resultText});
+        @required this.interpretation,
+        @required this.resultText});
 
   final String bmiResult;
   final String resultText;
@@ -17,54 +17,58 @@ class ResultsPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('BMI Calculator By Raj'),
-      ),
       body: Column(
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
-          Expanded(
-            child: Container(
-              padding: EdgeInsets.all(15.0),
-              alignment: Alignment.bottomLeft,
-              child: Text(
-                'Your Result',
-                style: kTitleTextStyle,
+          Container(
+            padding: EdgeInsets.only(top: 50),
+            alignment: Alignment.center,
+            child: Text(
+              'Your Result',
+              style: kTitleTextStyle,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 20,right: 20),
+            child: Neumorphic(
+              child: Expanded(
+                child: ReusableCard(
+                  colour: kActiveCardColour,
+                  cardChild: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        resultText.toUpperCase(),
+                        style: kResultTextStyle,
+                      ),
+                      Text(
+                        bmiResult,
+                        style: kBMITextStyle,
+                      ),
+                      Text(
+                        interpretation,
+                        textAlign: TextAlign.center,
+                        style: kBodyTextStyle,
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
           ),
-          Expanded(
-            flex: 5,
-            child: ReusableCard(
-              colour: kActiveCardColour,
-              cardChild: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Text(
-                    resultText.toUpperCase(),
-                    style: kResultTextStyle,
-                  ),
-                  Text(
-                    bmiResult,
-                    style: kBMITextStyle,
-                  ),
-                  Text(
-                    interpretation,
-                    textAlign: TextAlign.center,
-                    style: kBodyTextStyle,
-                  ),
-                ],
-              ),
-            ),
+          Container(
+              margin: EdgeInsets.all(50),
+              child: Neumorphic(
+                child:  BottomButton(
+                  buttonTitle: 'Re-Calculate',
+                  onTap: () {
+                    Navigator.pop(context);
+                  },
+                ),
+              )
           ),
-          BottomButton(
-            buttonTitle: 'Re-Calculate',
-            onTap: () {
-              Navigator.pop(context);
-            },
-          )
         ],
       ),
     );
